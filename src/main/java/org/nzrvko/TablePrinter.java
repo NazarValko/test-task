d2ecced697589c67c6c2720bcf3af4394ee05710
Nazar Valko <nazarvlk793@gmail.com>
2024-11-18 17:41:39 +0200

    Add code

--- src/main/java/org/nzrvko/TablePrinter.java
+++ src/main/java/org/nzrvko/TablePrinter.java
@@ -1,2 +1,23 @@
-package org.nzrvko;public class TablePrinter {
+package org.nzrvko;
+
+import java.util.List;
+
+public class TablePrinter {
+
+    public void createTable(List<String> strings, int columnCount) {
+        if (columnCount <= 0) throw new IllegalArgumentException("Column count must be greater than 0");
+
+        int columnWidth = strings.stream().mapToInt(String::length).max().orElse(0) + 5;
+        int totalCells = ((strings.size() + columnCount - 1) / columnCount) * columnCount;
+
+        for (int i = 0; i < totalCells; i++) {
+            String cellContent = i < strings.size() ? strings.get(i) : "";
+            System.out.print(padString(cellContent, columnWidth));
+            if ((i + 1) % columnCount == 0) System.out.println();
+        }
+    }
+
+    private String padString(String s, int width) {
+        return String.format("%-" + width + "s", s);
+    }
 }
